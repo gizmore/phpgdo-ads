8e504abe80c21bdb51ed6afdb7347ceed464d872
gizmore <gizmore@wechall.net>
2022-10-08 04:53:46 +0200

    '7.0.1-r1659 js fixes'

--- js/gdo7-ads.js
+++ js/gdo7-ads.js
@@ -1,10 +1,7 @@
 "use strict";
-
-window.GDO |= {};
-window.GDO.Ads |= {};
-
+window.GDO = window.GDO||{};
+window.GDO.Ads = {};
 window.GDO.Ads.cycle = function() {
 	console.log('Ads.cycle()');
 };
-
-setTimeout(window.GDO.Ads.cycle, )
+setTimeout(window.GDO.Ads.cycle, 30000);
